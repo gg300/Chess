e489fd1213c6e6703dd609e098943fd65e4d7a34
gg300 <georgebot300@yahoo.com>
2025-03-18 00:36:34 +0200

    too tired

--- main.c
+++ main.c
@@ -4,19 +4,38 @@
 unsigned int board[8][8];
 
 struct pieces {
-  char type;
   char color[6];
+  char type;
+  int type_int;
+  int x,y;
 };
 
+int initialize_board(int board[][8],struct pieces black[],struct pieces white[]){
+  for(int i=0;i<8;i++){
+    for(int j=0;j<8;j++);
+    //don't like the implementation
+      
+  
+}
+}
+
 int main(){
-    struct pieces black[2][8];
-    struct pieces white[2][8];
+    struct pieces black[16];
+    struct pieces white[16];
+
+    black[0].x=0;
+    black[0].y=0;
+    black[0].type='r';
+    black[0].type_int=1;
+  
+    board[black[0].x][black[0].y]=black[0].type_int;
+
     for(int i=0;i<8;i++){
       printf("\n");
       for(int j=0;j<8;j++){
         printf("%d ",board[i][j]);
       }
     }
-//;
+
     return 0;
 }
