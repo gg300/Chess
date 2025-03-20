eaba3a85e5f3a9590699e7c66a2c4bf9055398a2
gg300 <georgebot300@yahoo.com>
2025-03-20 18:38:16 +0200

    started moves

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 unsigned int board[8][8];
 
 struct pieces {
@@ -50,8 +51,6 @@ void initialize_board(unsigned int board[][8],struct pieces black[],struct piece
       white[i].type_int=1;
       black[i].type='p';
       white[i].type='p';
-      
-      
     }
     if(i<8){
     
@@ -91,12 +90,10 @@ void initialize_board(unsigned int board[][8],struct pieces black[],struct piece
         white[i].y=i-8;
       }
     }
-    
-    
   }
 }
 
-void plm(unsigned board[][8],struct pieces black[],struct pieces white[]){
+void show(unsigned board[][8],struct pieces black[],struct pieces white[]){
   for(int i=0;i<8;i++){
     printf("\n");
     for(int j=0;j<8;j++){
@@ -116,22 +113,72 @@ void plm(unsigned board[][8],struct pieces black[],struct pieces white[]){
           }  
         }
       }
-        if(is_piece==0)
+        if(is_piece==0){
+
           if((i+j)%2==0)
             printf("%c ", ' ');
           else
             printf("%c ", '#');
+          }
     }
   }
 }
+
+void show_int_board(unsigned board[][8]){
+  for(int i=0;i<8;i++){
+    printf("\n");
+    for(int j=0;j<8;j++){
+      printf("%d ",board[i][j]);
+    }
+  }
+ }
+int is_valid_move(struct pieces piece,int x, int y,unsigned board[][8]){ //ocupied spaces not verified yet
+  if(x<0||x>7||y<0||y>7)
+    return false;
+
+
+  if(piece.type=='p'){ // 1
+    return 1;
+  }
+  if(piece.type=='b'){  // 2
+    for(int i=0;i<8;i++){
+      if((piece.x+i==x && piece.y+i==y) || (piece.x-i==x && piece.y-i==y))
+          return true;
+//      if(piece.x+i==x && piece.y)
+    }
+    return 2;
+  }
+  if(piece.type=='h'){ // 3
+    if(((piece.x+2==x) && (piece.y+1==y)) || ((piece.x+2==x) && (piece.y-1==y)) || ((piece.x-2==x) && (piece.y-1==y)) || ((piece.x-2==x) && (piece.y+1==y)))
+      return true;
+    return false;
+  }
+  if(piece.type=='r'){ // 4
+    for(int i=0;i<8;i++)
+      if((piece.x+i==x && piece.y==y) || (piece.y+i==y && piece.x==x) || (piece.x-i==x && piece.y==y) || (piece.x==x && piece.y-i==y))
+         return true;
+//    printf("%d%d ",piece.x,piece.y);
+    return false;
+  }
+  if(piece.type=='q'){ // 5
+    return 5;
+  }
+  if(piece.type=='k'){ // 6
+    return 6;
+  }
+
+  return false;
+}
+
+
 
 int main(){
     int player=0;
     struct pieces black[16];
     struct pieces white[16];
     initialize_board(board,black,white,player);
-    
-    plm(board,black,white);
-
+    printf("%d",is_valid_move(white[2],6,1,board));
+//    show(board,black,white);
+    show_int_board(board);
     return 0;
 }
